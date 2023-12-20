72168273caf8e9ca8f29a7bdfc940f3585c73154
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-12-19 21:20:43 -0300

    Criando projeto

---
+++ src/main/java/br/com/lucassantos/todolistajava/TodolistajavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucassantos.todolistajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistajavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistajavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucassantos/todolistajava/TodolistajavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucassantos.todolistajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistajavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
